e8baf3ee0cc19b747c042b43fe1df2b926536f3c
MaguiML <mserranoc0800@alumno.ipn.mx>
2024-01-16 17:59:00 -0500

    copia de seguridad de tmp

--- backup-utility.sh
+++ backup-utility.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Directorio de origen que deseas respaldar
-origen="/ruta/al/directorio/a/respaldar"
+# Directorio de origen que deseas respaldar: aqui será el tmp
+origen="/tmp"
 
 # Directorio de destino donde se almacenarán las copias de seguridad
-destino="/ruta/al/directorio/de/respaldo"
+destino="/home/kali/Documents/Respaldos"
 
 # Nombre del archivo de copia de seguridad con marca de tiempo
 archivo_destino="$destino/respaldo_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
